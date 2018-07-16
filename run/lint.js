ec0a8ff948897bc2ea600789e23cc340fc5619be
Andrew Balakirev <balakirev.andrey@gmail.com>
2018-07-16 03:47:32 +0300

    Minimal boilerplate for micro services - Eslint, prettier and precommit hook with husky as a single dependency (#1)
    * eslint, prettier and precommit hook with husky as simngle dependency
    
    * v1.0.0-alpha.2
    
    * v1.0.0-alpha.3
    
    * v1.0.0-alpha.4
    
    * nodemon added
    
    * fix linting renamed files
    
    * v1.0.0-alpha.5
    
    * Some docs
    
    * v1.0.0

---
+++ run/lint.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const path = require("path");
+const { readAccess } = require("plyo-tools/lib/fs");
+const { spawn, exec } = require("plyo-tools/lib/childProcess");
+
+function fileInOriginRepo(fileName) {
+  return readAccess(path.resolve(process.cwd(), fileName));
+}
+
+async function lint(staged) {
+  const args = ["eslint"];
+
+  let stagedFiles = [];
+  if (staged) {
+    // we need `|| true` at the end because grep return 1 exit status if no lines matched and the script throws an error
+    const { stdout } = await exec(
+      // $4 -> name of renamed file
+      // $2 -> name of changed file
+      // in case of renaming we should use destination file, since source one does not exist
+      "git status -s -uno | grep -v D |  grep -v '^ ' | awk '$4{print $4} !$4{print $2}' | grep js$ || true"
+    );
+    stagedFiles = stdout.split("\n");
+    stagedFiles.pop();
+  }
+
+  if (!stagedFiles.length) {
+    return;
+  }
+
+  if (
+    !await fileInOriginRepo(".eslintrc") &&
+    !await fileInOriginRepo(".eslintrc.js")
+  ) {
+    args.push("--config", path.resolve(__dirname, "../.eslintrc.js"));
+  }
+
+  if (staged) {
+    args.push("--fix", "--no-ignore", ...stagedFiles);
+  } else {
+    args.push("--ignore-path", ".gitignore", ".");
+  }
+
+  await spawn("npx", args, {
+    env: process.env,
+    cwd: process.cwd(),
+    stdio: "inherit"
+  });
+
+  if (staged) {
+    await spawn("git", ["add", "--force", ...stagedFiles], {
+      env: process.env,
+      cwd: process.cwd(),
+      stdio: "inherit"
+    });
+  }
+}
+
+lint(process.argv.includes("--staged")).then(
+  () => console.log("Lint completed"), // eslint-disable-line no-console
+  e => {
+    console.error(e); // eslint-disable-line no-console
+    process.exit(1);
+  }
+);
